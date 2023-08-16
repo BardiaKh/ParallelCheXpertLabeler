0e52c56e0c235270628b796102882365f1dd1367
Bardia Khosravi <bardiakhosravi95@gmail.com>
2023-08-15 21:43:11 -0500

    Update label_concatenation.py

--- label_concatenation.py
+++ label_concatenation.py
@@ -8,7 +8,7 @@ processed_files = glob.glob(INPUT_DF_PATH.replace(".csv", "*_labeled.csv"))
 processed_files.sort()
 final_df = pd.DataFrame()
 for file in tqdm(processed_files):
-    df = pd.read_csv(file)[[REPORT_COL, ID_COLUMN] + CATEGORIES]
+    df = pd.read_csv(file)#[[REPORT_COL, ID_COLUMN] + CATEGORIES]
     final_df = pd.concat([final_df, df], ignore_index=True)
     
 final_df.to_csv(OUTPUT_DF_PATH, index=False)
